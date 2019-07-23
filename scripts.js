81c4126a10c5d953fa584c4bac0228f5a23b0f93
smv5047 <smv5047@gmail.com>
2019-07-22 22:03:50 -0400

    attempted to add timer

--- scripts.js
+++ scripts.js
@@ -81,4 +81,21 @@ function resetBoard() {
     });
 })();
 
+window.onload = function() {
+    var hour = 2;
+    var sec = 60;
+    setInterval(function() {
+      document.getElementById("timer").innerHTML = hour + " : " + sec;
+      sec--;
+      if (sec == 00) {
+        hour--;
+        sec = 60;
+        if (hour == 0) {
+          hour = 2;
+        }
+      }
+    }, 1000);
+  }
+
+
 cards.forEach(cards => cards.addEventListener('click', flipCard))
